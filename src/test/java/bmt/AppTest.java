92e132c6b1aa2c7cdea950e73b667b5283e7a25d
RafiAR <56111747+RafiRidel123@users.noreply.github.com>
2023-06-16 00:21:56 +0700

    Update AppTest.java

--- src/test/java/bmt/AppTest.java
+++ src/test/java/bmt/AppTest.java
@@ -9,12 +9,31 @@ import org.junit.Test;
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testAddition() {
+        String input = "5\n3\n1\n";
+        String expectedOutput = "Hasil: 8.0\n";
+        testAppWithInput(input, expectedOutput);
     }
+
+    @Test
+    public void testSubtraction() {
+        String input = "8\n4\n2\n";
+        String expectedOutput = "Hasil: 4.0\n";
+        testAppWithInput(input, expectedOutput);
+    }
+
+    @Test
+    public void testMultiplication() {
+        String input = "6\n2\n3\n";
+        String expectedOutput = "Hasil: 12.0\n";
+        testAppWithInput(input, expectedOutput);
+    }
+
+    @Test
+    public void testDivision() {
+        String input = "10\n5\n4\n";
+        String expectedOutput = "Hasil: 2.0\n";
+        testAppWithInput(input, expectedOutput);
+    }
 }
