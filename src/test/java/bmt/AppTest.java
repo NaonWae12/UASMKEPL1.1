273a0e14a4bbbd444e73105a53d72139dda8748b
hendrianiyan <100855337+hendrianiyan@users.noreply.github.com>
2023-06-16 01:13:49 +0700

    Update AppTest.java

--- src/test/java/bmt/AppTest.java
+++ src/test/java/bmt/AppTest.java
@@ -1,39 +1,79 @@
 package bmt;
 
-import static org.junit.Assert.assertTrue;
-
+import org.apache.commons.lang3.math.NumberUtils;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+public class AppTest {
+    private final InputStream systemIn = System.in;
+    private final PrintStream systemOut = System.out;
+    private ByteArrayInputStream testIn;
+    private ByteArrayOutputStream testOut;
+
+    @Before
+    public void setUpOutput() {
+        testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+    }
+
+    @After
+    public void restoreSystemInputOutput() {
+        System.setIn(systemIn);
+        System.setOut(systemOut);
+    }
+
+    private void provideInput(String data) {
+        testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
     @Test
     public void testAddition() {
-        String input = "5\n3\n1\n";
-        String expectedOutput = "Hasil: 8.0\n";
-        testAppWithInput(input, expectedOutput);
+        provideInput("5\n10\n1\n");
+        App.main(new String[]{});
+        assertEquals("Hasil: 15.0\n", testOut.toString());
     }
 
     @Test
     public void testSubtraction() {
-        String input = "8\n4\n2\n";
-        String expectedOutput = "Hasil: 4.0\n";
-        testAppWithInput(input, expectedOutput);
+        provideInput("15\n7\n2\n");
+        App.main(new String[]{});
+        assertEquals("Hasil: 8.0\n", testOut.toString());
     }
 
     @Test
     public void testMultiplication() {
-        String input = "6\n2\n3\n";
-        String expectedOutput = "Hasil: 12.0\n";
-        testAppWithInput(input, expectedOutput);
+        provideInput("3\n6\n3\n");
+        App.main(new String[]{});
+        assertEquals("Hasil: 18.0\n", testOut.toString());
     }
 
     @Test
     public void testDivision() {
-        String input = "10\n5\n4\n";
-        String expectedOutput = "Hasil: 2.0\n";
-        testAppWithInput(input, expectedOutput);
-    }
+        provideInput("20\n4\n4\n");
+        App.main(new String[]{});
+        assertEquals("Hasil: 5.0\n", testOut.toString());
+    }
+
+    @Test
+    public void testInvalidInput() {
+        provideInput("Hello\n");
+        App.main(new String[]{});
+        assertEquals("Input tidak valid!\n", testOut.toString());
+    }
+
+    @Test
+    public void testInvalidChoice() {
+        provideInput("10\n5\n5\n");
+        App.main(new String[]{});
+        assertEquals("Pilihan tidak valid!\n", testOut.toString());
+    }
 }
